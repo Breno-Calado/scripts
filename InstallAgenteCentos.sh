53a6f5a5f466be777233dd9d1d652af936d24a59
Breno-Calado <breno.calado@gmail.com>
2020-10-13 22:17:30 -0300

    Update InstallAgenteCentos.sh

--- InstallAgenteCentos.sh
+++ InstallAgenteCentos.sh
@@ -8,7 +8,7 @@ sudo yum install zabbix-agent -y
 sudo echo PidFile=/var/run/zabbix/zabbix_agentd.pid > /etc/zabbix/zabbix_agentd.conf
 sudo echo LogFile=/var/log/zabbix/zabbix_agentd.log >> /etc/zabbix/zabbix_agentd.conf
 sudo echo LogFileSize=0 >> /etc/zabbix/zabbix_agentd.conf
-sudo echo Server=mysqlPassword >> /etc/zabbix/zabbix_agentd.conf
+sudo echo Server=$mysqlPassword >> /etc/zabbix/zabbix_agentd.conf
 sudo echo Hostname=hostName >> /etc/zabbix/zabbix_agentd.conf
 sudo systemctl enable zabbix-agent
 sudo systemctl start zabbix-agent
